11cff6ac75c5728b72e22f478fbeb4efe3af86b0
zainnadeem <zn.nadeem@gmail.com>
2016-07-08 10:51:18 -0400

    optionals

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -16,3 +16,7 @@ if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
 }
+
+
+
+//Another thing to note: When unwrapping Optionals, you don't need to have an else branch in your if statement. If the Optional is nil and you didn't write an else branch, nothing will happen. Sometimes you want to do nothing, and sometimes you want to do something different if the Optional is nil. Either way, Swift forces you to deal with nil values.
